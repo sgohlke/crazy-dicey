4bfc8f89271dd6c01df0c270c9ee9f3944524117
stefangohlke <gohlke.stefan@yahoo.de>
2022-10-08 02:17:10 +0200

    Rename and add export index

---
+++ src/CrazyDicey.ts
@@ -0,0 +1,54 @@
+import { Player } from "./Player"
+
+export const NUMBER_MUST_BE_POSITIVE_ERROR = new Error('Number of sides should be positive')
+export const ROUNDS_MUST_BE_POSITIVE_ERROR = new Error('Number of rounds should be positive')
+
+
+export class CrazyDicey {
+    diceSides: number
+    player: Player
+
+    constructor(diceSides = 6, player = new Player('player')) {
+        this.player = player;
+        if (diceSides && diceSides > 0) {
+            this.diceSides = diceSides
+        } else {
+            throw NUMBER_MUST_BE_POSITIVE_ERROR
+        }
+    }
+
+    generateRandomDiceRoll(): number {
+        const randomNumber = Math.random() * this.diceSides
+        return Math.ceil(randomNumber);
+    }
+
+    rollDice(numberOfRounds = 1): Array<number> {
+        const results: Array<number> = []
+        for (let round = 0; round < numberOfRounds; round++) {
+            results.push(this.generateRandomDiceRoll())            
+        }
+        return results
+    }
+
+    rollDiceAndSumResults(numberOfRounds = 1): number {
+        if (numberOfRounds < 0) {
+            throw ROUNDS_MUST_BE_POSITIVE_ERROR
+        }
+
+        let sum = 0
+        for (let index = 0; index < numberOfRounds; index++) {
+            sum += this.generateRandomDiceRoll()
+        }
+        return sum
+
+    }
+
+    rollDiceOnce(): number {
+        return this.generateRandomDiceRoll()
+    }
+
+    sayGoodbye(): void {
+        console.info('Goodbye')
+        process.exit(0)
+    }
+}
